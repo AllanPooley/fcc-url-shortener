9046aa62779b41de8f8bceb22d6862df48285793
Allan Pooley <allanpooley@outlook.com>
2017-04-24 06:22:54 +0000

    Saving new urls and incrementing ids implemented!

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
+var crypto = require("crypto");
 
 var PORT = 8080;
 var DB_NAME = 'fcc-url-shortener';
-var COLLECTION_NAME = 'urls'
+var DB_COLLECTION_NAME = 'urls'
 var DB_URL = 'mongodb://quincy:larson@ds163940.mlab.com:63940/' + DB_NAME;
 var DOMAIN = 'https://fcc-url-shortener-allanpooley.c9users.io';
 
@@ -14,6 +15,21 @@ app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+
+var db;
+
+MongoClient.connect(DB_URL, (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(PORT, function (){
+    console.log('Listening on port ' + PORT);
+  });
+  
+});
+
+
+
 app.get('/', function(req, res) {
     
   // Render 'About' style home page.
@@ -21,23 +37,20 @@ app.get('/', function(req, res) {
   
 });
 
+
+// *** NOTE: Issues with parsing https://, http:// <-- May need regex
 app.get('/new/:url', function(req, res) {
   
   // Get the full url passed by the user that needs to be shortened.
   var url = req.params.url;
   
-  // Save URL in database, getting shortened URL
-  var extension = saveURL(url);
-  
   // Responds with JSON
-  res.send( 
-    { "original_url": url, 
-      "short_url": DOMAIN + '/' + extension }
-  );
+  res.send(saveURL(url));
   
 });
 
 app.get('/:id', function(req, res) {
+   
   
   // Get the short url id passed by the user on the tail end of the url.
   var id = req.params.id;
@@ -50,22 +63,42 @@ app.get('/:id', function(req, res) {
   
 });
 
-app.listen(PORT, function (){
-  console.log('Listening on port ' + PORT);
-});
+function readURL(id) {
+  return { "original_url": "test", "short_url": "test" };
+}
+
 
 
 // Saves a new URL to the database returning the new extension on this domain (shortened URL)
 function saveURL(url) {
-  return "1";
+  
+  getNextSequence('urlid', function(err, seq) {
+    if (err) console.error(err);
+    
+    
+    var newUrl = {
+      "original_url": url,
+      "short_url": DOMAIN + '/' + seq
+    };
+    
+    db.collection(DB_COLLECTION_NAME).insert(newUrl, function(err, result) {
+        if (err) console.error(err);
+        
+        return newUrl;
+    });
+    
+  });
 }
 
-// Retrieves a url saved in the database and returns 
-function readURL(id) {
-  return "www.google.com.au";
-}
-
-function countURLs() {
-  return 0;
+function getNextSequence(name, callback){
+    db.collection("counters").findAndModify(
+        { "_id": name },
+        [],
+        { "$inc": { "seq": 1 }},
+        { "new": true, "upsert": true },
+        function(err, result) {
+          callback( err, result.value.seq );
+        }
+    );
 }
 
